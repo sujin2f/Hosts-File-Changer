967ddcd2361e7965455aef97f700490466be5db6
Sujin <sujin.2f@gmail.com>
2015-08-26 19:12:09 -0400

    Initial Commit

---
+++ Hosts File Changer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hosts File Changer
+//
+//  Created by Sujin on 2015-08-26.
+//  Copyright (c) 2015 Sujin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
